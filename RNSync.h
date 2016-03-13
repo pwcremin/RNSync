aab1b92507df3d7e7a4c1c7d07b1be63a72dee2d
Patrick Cremin <pwcremin@gmail.com>
2016-03-13 13:22:37 -0500

    extra error handling for data retrieve failure

---
+++ RNSync.h
@@ -0,0 +1,13 @@
+//
+//  ReactSync.h
+//  reactCloudantSync
+//
+//  Created by Patrick cremin on 2/17/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+#import "CloudantSync.h"
+
+@interface RNSync : NSObject <RCTBridgeModule, CDTReplicatorDelegate>
+@end
